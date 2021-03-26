dac9f3537a29182321ab2eebfc9347a2cbd195ee
DESKTOP-S09O801\shigo <shigoto922@gmail.com>
2021-03-26 22:57:47 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const App = () => {
   const [flag, setFlag] = useState(true)
   const [message, setMessage] = useState()
   const [winner, setWinner] = useState('')
+  const [stepNumber, setStepNumber] = useState(0)
   const [history, setHistory] = useState({
     history: [
       {
@@ -28,12 +29,18 @@ const App = () => {
   })
 
   useEffect(() => {
+    const squares = document.querySelectorAll('.square')
+    for (const square of squares) {
+      square.removeAttribute('id')
+    }
+
+    console.log(history)
+    console.log(stepNumber)
     const arrayIndex = []
     const stone = blackIsNext ? '○' : '●'
-    const squares = document.querySelectorAll('.square')
     // eslint-disable-next-line
     const [rowIndex, colIndex, hougaku] = check(
-      history.history[0].square,
+      history.history[stepNumber].square,
       stone
     )
 
@@ -64,21 +71,26 @@ const App = () => {
     setRowIndex(rowIndex)
     setColIndex(colIndex)
     setHougaku(hougaku)
+    // setStepNumber(history.length)
     // eslint-disable-next-line
   }, [blackIsNext])
 
   const status = `Next Player is ${blackIsNext ? 'white' : 'black'}`
   const handleClick = event => {
-    console.log(hougaku)
-    console.log(rowIndex)
-    console.log(colIndex)
-
     setMessage('')
+    console.log(stepNumber)
     const squares = document.querySelectorAll('.square')
     const stone = blackIsNext ? '○' : '●'
-    const slicedHistory = history.history.slice()
-    const currant = slicedHistory[slicedHistory.length - 1]
-    const square = currant.square
+    const slicedHistory = JSON.parse(
+      JSON.stringify(history.history.slice(0, stepNumber + 1))
+    )
+    // console.log(slicedHistory)
+    const currant = JSON.parse(
+      JSON.stringify(slicedHistory[slicedHistory.length - 1])
+    )
+    // console.log(currant)
+    const square = JSON.parse(JSON.stringify(currant.square.slice()))
+    // console.log(square)
     const index = getClickedIndex(event, squares)
     if (isCheckPutStonePlace(index, squares)) {
       setWinner('')
@@ -96,15 +108,30 @@ const App = () => {
         square,
         stone
       )
-      setHistory({ history: [{ square: changedSquare }] })
+      // console.log(changedSquare)
+      console.log(history)
+      setHistory({ history: slicedHistory.concat([{ square: changedSquare }]) })
       setBlackIsNext(!blackIsNext)
       setFlag(true)
+      setStepNumber(history.history.length)
     }
   }
+
+  const jump = () => {
+    const step = stepNumber - 1
+    setStepNumber(step)
+    setBlackIsNext(stepNumber % 2 === 0)
+  }
+
   return (
     <div className='game'>
       <div className='game-board'>
-        <Board value={history.history[0].square} onClick={handleClick} />
+        {console.log(stepNumber)}
+        {console.log(history.history)}
+        <Board
+          value={history.history[stepNumber - 1].square}
+          onClick={handleClick}
+        />
       </div>
       <div className='game-info'>
         <div>{status}</div>
@@ -112,6 +139,7 @@ const App = () => {
         <p>黒の石の数:{blackStoneCount}</p>
         <p>白の石の数:{whiteStoneCount}</p>
         <p>{winner}</p>
+        <button onClick={jump}>待った</button>
       </div>
     </div>
   )
